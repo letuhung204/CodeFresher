7510210c4421fb7ff3c9ea7466c02ed2ceccb4ef
letuhung204 <tuhung204@gmail.com>
2021-05-28 22:48:33 +0700

    code jpa

---
+++ buoi14/buoi14-springjpa/src/main/java/com/example/demo/DTO/StudentResponse.java
@@ -0,0 +1,25 @@
+package com.example.demo.DTO;
+
+import java.util.List;
+
+public class StudentResponse {
+	
+	private String firstName;
+	
+	
+	public StudentResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+
+}
---
+++ buoi14/buoi14-springjpa/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,89 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.hibernate.type.SerializableType;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="student")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Student implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private long id;
+	
+	@Column(name="firstName")
+	private String firstName;
+
+	
+	private String lastName;
+	
+	private String age;
+	
+	private String lop;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getLop() {
+		return lop;
+	}
+
+	public void setLop(String lop) {
+		this.lop = lop;
+	}
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+
+	
+}
